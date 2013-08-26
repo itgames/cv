978ac0b586549102c31fe4d124b54ef5b492b0c7
sk-y <sk-y@live.ru>
2013-08-27 02:34:58 +0700

    test app created

---
+++ src/main/main/app/cv/Main.java
@@ -0,0 +1,45 @@
+package app.cv;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.nio.channels.SelectableChannel;
+
+/**
+ * @author: sk_y
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server =new Server();
+        server.setThreadPool(createThreadPool());
+        server.setConnectors(createConnectors());
+        WebAppContext webapp = new WebAppContext();
+        String path =  webapp.getClass().getClassLoader().getResource("").getPath();
+        webapp.setResourceBase(path+"/..");
+        webapp.setDescriptor("WEB-INF/web.xml");
+        webapp.setContextPath("/");
+        server.setHandler(webapp);
+        server.start();
+    }
+
+    private static Connector[] createConnectors() {
+        return new Connector[]{createDefaultConnector()};
+    }
+
+    private static Connector createDefaultConnector() {
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(8080);
+        return connector;
+
+    }
+
+    private static ThreadPool createThreadPool() {
+        QueuedThreadPool threadPool = new QueuedThreadPool(100);
+        threadPool.setMinThreads(10);
+        return threadPool;
+    }
+}
